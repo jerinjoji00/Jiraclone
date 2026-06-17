8bb43ca047293c5dd486336a341b95d2d1ef8c85
Jerin Joji <jerinjoji00@gmail.com>
2026-06-17 21:42:20 +0530

    Update clientlayout.tsx

--- client/components/clientlayout.tsx
+++ client/components/clientlayout.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 const clientlayout = () => {
     const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(true);
     return (
-        <div>clientlayout</div>
+        <div></div>
     )
 }
 
