9c05e3510b03ba665b52dc319b085a6507048d0a
Jerin Joji <jerinjoji00@gmail.com>
2026-06-25 23:43:35 +0530

    main

--- client/components/clientlayout.tsx
+++ client/components/clientlayout.tsx
@@ -1,10 +1,21 @@
+"use client";
+
+import { Sidebar } from 'lucide-react';
 import React from 'react'
 import { useState } from 'react'
-const clientlayout = () => {
+const clientlayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
     const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(true);
-    return (
-        <div>clientlayout</div>
-    )
+    if (isAuthenticated === null) {
+        return <div className="h-screen w-full flex items-center justify-center">
+            <p>loading...</p>
+        </div>;
+    }
+    return <div className="flex min-h-screen bg-white">
+        <Sidebar />
+        <main className="flex-1 overflow-x-hidden">{children}</main>
+    </div>;
+
+
 }
 
 export default clientlayout
