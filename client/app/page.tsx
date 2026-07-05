ac506a4cfef719dc7f0e7ccbde9758a70e00a113
Jerin Joji <jerinjoji00@gmail.com>
2026-07-05 21:34:27 +0530

    Update page.tsx

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-rt default function Home() {
+export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
     </div>
