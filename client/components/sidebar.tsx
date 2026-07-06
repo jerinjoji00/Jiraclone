142172181f5f955edfee38dfc67c07f52283269d
Jerin Joji <jerinjoji00@gmail.com>
2026-07-06 23:00:43 +0530

    Update sidebar.tsx

--- client/components/sidebar.tsx
+++ client/components/sidebar.tsx
@@ -5,7 +5,7 @@ const sidebar = () => {
     <aside className="w-64 border-r border-zinc-200 bg-white px-4 py-4">
       <div className="text-left text-xl font-semibold text-zinc-900">
         Jira Clone
-      </div>vvv
+      </div>
     </aside>
   )
 }
